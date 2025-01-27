a75c99bf681e0f7ef7101e4f40908918315db353
EliseAld <40570458+EliseAld@users.noreply.github.com>
2025-01-27 18:27:47 +0100

    Update supp3_supp4.R

--- ranking_figures/figures/supp_figs/supp3_supp4.R
+++ ranking_figures/figures/supp_figs/supp3_supp4.R
@@ -5,7 +5,7 @@ date = "241025"
 score_path = "../../compute_metrics/scores/"
 source("../generic_functions/load_scores_SB_silico.R")
 source("../generic_functions/ranking_process.R")
-folder = strsplit(basename(rstudioapi::getSourceEditorContext()$path),".R")[[1]]
+folder = "supp3_supp4"
 
 meth_rna_sup = c("DeconRNASeq", "nnls", "ols","svr","CIBERSORT", "elasticnet", "rlr","WISP", "InstaPrism", "fardeep", "fardeepsto")
 meth_rna_unsup = c("ICA", "NMF", "PREDE", "debCAM", "CDSeq")

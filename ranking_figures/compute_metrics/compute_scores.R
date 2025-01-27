0e3ebfb8a217a73a84a90d2954b9fa03895c3bc9
EliseAld <40570458+EliseAld@users.noreply.github.com>
2025-01-27 17:44:41 +0100

    Update compute_scores.R

--- ranking_figures/compute_metrics/compute_scores.R
+++ ranking_figures/compute_metrics/compute_scores.R
@@ -3,7 +3,7 @@ source("generic_functions_metrics.R")
 ## ----
 ## Set parameters, change with your own
 ## ----
-date <- "241025"
+date <- "240101"
 silico_path <- "../../data/simulations/"
 vitro_path <- "../../data/invitro/"
 vivo_path <- "../../data/invivo/"
@@ -11,18 +11,18 @@ deconv_path <- "../../deconvolution/results/prediction/"
 timing_path <- "../../deconvolution/results/timing/"
 score_path <- "scores/"
 score_methods <- c("rmse", "mae", "pearson")
-best2_deconv_methods <- data.frame(DeconvTool=c('rlr','nnls','ICA','NMF','rlr','WISP','debCAM','PREDE'),
-                                   FS='none',
-                                   Block=c(rep('dnam',4),rep('rna',4)),
-                                   Supervised=rep(c(rep('sup',2),rep('unsup',2)),2))
+#best2_deconv_methods <- data.frame(DeconvTool=c('rlr','nnls','ICA','NMF','rlr','WISP','debCAM','PREDE'),
+#                                   FS='none',
+#                                   Block=c(rep('dnam',4),rep('rna',4)),
+#                                   Supervised=rep(c(rep('sup',2),rep('unsup',2)),2))
 
 ## ----
 ## Scores
 ## ----
 score_SB_silico(silico_path, deconv_path, score_path,
                   date, score_methods)
-score_SB_consensus(silico_path, deconv_path, score_path,
-                date, best2_deconv_methods, score_methods)
+#score_SB_consensus(silico_path, deconv_path, score_path,
+#                date, best2_deconv_methods, score_methods)
 score_SB_invitro(vitro_path, deconv_path, score_path,
                    score_methods)
 score_SB_invivo(vivo_path, deconv_path, score_path,
